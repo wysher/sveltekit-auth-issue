096ca0cd4e50d1938d104bb4c63fbc7526a81f7e
Mariusz Wachowski <wachowski.mariusz@gmail.com>
2022-03-06 21:41:23 +0100

    fix(client): use accept header to check ajax requests

--- src/routes/client/auth/logout.ts
+++ src/routes/client/auth/logout.ts
@@ -1,17 +1,28 @@
 import cookie from 'cookie';
 import { LOGIN_URL } from '../_constants';
 
-export async function post() {
-    return {
-        status: 200,
-        body: { userid: '' },
-        headers: {
-            location: LOGIN_URL,
-            'Set-Cookie': cookie.serialize('userid', '', {
-                path: '/',
-                expires: new Date(0),
-                httpOnly: true,
-            })
-        }
+export async function post({ request }) {
+	const headers = {
+		'Set-Cookie': cookie.serialize('userid', '', {
+			path: '/',
+			expires: new Date(0),
+			httpOnly: true
+		})
+	};
+
+    if (request.headers.get('accept') === 'application/json') {
+        return {
+            status: 200,
+            body: { userid: 1 },
+            headers,
+        };
     }
+
+	return {
+		status: 302,
+		headers: {
+			...headers,
+			location: LOGIN_URL
+		}
+	};
 }
